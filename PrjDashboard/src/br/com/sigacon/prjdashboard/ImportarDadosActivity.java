5d0ff8ad95ca9180380e1d9bd108bcc8166846d0
Charleston Augusto Miszczak Parapinski <sigacon@sigacon.com.br>
2013-10-31 16:18:07 -0200

    Trocado o id da view EditText em importar_dados.xml para fileName Importada a library fileChooser e implementada no projeto

--- PrjDashboard/src/br/com/sigacon/prjdashboard/ImportarDadosActivity.java
+++ PrjDashboard/src/br/com/sigacon/prjdashboard/ImportarDadosActivity.java
@@ -1,11 +1,17 @@
 package br.com.sigacon.prjdashboard;
 
+import java.util.ArrayList;
+
+import br.com.thinkti.android.filechooser.FileChooser;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 import android.support.v7.app.ActionBarActivity;
 	
 public class ImportarDadosActivity extends ActionBarActivity {
-
+	private static final int FILE_CHOOSER =  10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);                
@@ -16,6 +22,23 @@ public class ImportarDadosActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.importar, menu);
         return true;
+    }
+    public void chooseFile(View view)
+    {
+    	 Intent intent = new Intent(this, FileChooser.class);
+    	 ArrayList<String> extensions = new ArrayList<String>();
+    	 //extensions.add(".pdf");
+    	 extensions.add(".xls");   
+    	 extensions.add(".xlsx");
+    	 intent.putStringArrayListExtra("filterFileExtension", extensions);
+    	 startActivityForResult(intent, FILE_CHOOSER);
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == FILE_CHOOSER && data != null) {
+            String fileSelected = data.getStringExtra("fileSelected");
+            EditText fileName = (EditText)findViewById(R.id.fileName);
+            fileName.setText(fileSelected);
+        }                  
     }
        
 }
